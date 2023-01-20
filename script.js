f447be7bc485e4e7a24c02418591be1373ff8679
AlexZviadadze <117842458+AlexZviadadze@users.noreply.github.com>
2023-01-20 10:05:07 +0400

    full script

---
+++ script.js
@@ -0,0 +1,25 @@
+// 4
+for (var i=0; i < 5; i++){
+    let numbers = 123
+    console.log(numbers);
+}
+
+// 1-2
+
+let fisrtName = 'Alex';
+let lastName = 'Zviadadze';
+let age = 25;
+
+let fullName = "My name is"  + " "+ fisrtName + " " + lastName;
+let MyAge = age +" "+ "years old";
+console.log(`${fullName} ${MyAge}`);
+
+
+// 3
+
+let age = 25;
+if(age > 19) {
+    console.log("I am an adult");
+}else if(age < 19) {
+    console.log("I am teenager");
+};
